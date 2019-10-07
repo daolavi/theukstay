724f0a0b1ce0be274b050756379e46b65b9dd342
Dao Lam <daolavi@gmail.com>
2019-10-07 22:56:38 +0700

    Create website for counting down the date leaving The UK

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <Countdown date="October 28, 2019"></Countdown>
+  </div>
+</template>
+
+<script>
+import Countdown from './components/Countdown.vue';
+
+export default {
+    components: { Countdown },
+}
+</script>
+
+<style>
+#app{
+    align-items: center;
+    bottom: 0;
+    background-color: #292b33;
+    display: flex;
+    justify-content: center;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top:0;
+}
+</style>
