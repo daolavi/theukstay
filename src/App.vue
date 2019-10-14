1f179a6252001efc2f953551a1d8869209fa8df6
Dao Lam <daolavi@gmail.com>
2019-10-14 10:57:37 +0700

    Add background

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,16 @@ export default {
 #app{
     align-items: center;
     bottom: 0;
-    background-color: #292b33;
     display: flex;
     justify-content: center;
     left: 0;
     position: absolute;
     right: 0;
     top:0;
+    background-color: #292b33;
+    background-image: url("background.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
 }
 </style>
